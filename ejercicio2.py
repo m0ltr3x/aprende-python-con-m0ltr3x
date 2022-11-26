033053b72700fe55a5c46bc9471ec0e394c6361e
m0ltr3x <119032735+m0ltr3x@users.noreply.github.com>
2022-11-26 16:06:59 +0100

    Delete ejercicio2.py

--- ejercicio2.py
+++
@@ -1,5 +0,0 @@
-##                                  EJERCICIO 2 | TEMA 1 | Tipos de Datos Simples
-##- Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
-
-nombre="hola soy m0ltr3x"
-print(nombre)
