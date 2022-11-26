f61a4a8a46046e700c5cde29f39f12a407948866
m0ltr3x <119032735+m0ltr3x@users.noreply.github.com>
2022-11-26 16:07:21 +0100

    Delete ejercicio4.py

--- ejercicio4.py
+++
@@ -1,6 +0,0 @@
-#                       EJERCICIO 4 | TEMA 1 | Tipos de Datos Simples
-#Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética  (3+22⋅5)2 .
-
-
-
-print(((3+2)/(2*5))**2)
